92a5f8364c8c840daabbd17fab6d1832c03b462d
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2020-11-12 22:53:53 +0200

    Update shoppingcart.dart

--- shoppingcart.dart
+++ shoppingcart.dart
@@ -1,10 +1,9 @@
-import 'dart:ui';
-
-import 'package:firebase/firebase.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:intel/responsive/responsiveHomePage.dart';
 import 'package:intel/widgets/item.dart';
-import 'package:intel/bottomnavbar/homapage.dart';
+import 'package:intel/material.dart';
+
+
+
 
 class ShoppingCart extends StatefulWidget {
   @override
@@ -13,83 +12,36 @@ class ShoppingCart extends StatefulWidget {
 
 class _ShoppingCartState extends State<ShoppingCart> {
 
-  List<String> itemName = ['BeoPlay Speaker', 'Leather Wristwatch','Chair ','Lamp' ,'Home','Office'];
-  List<String> itemImage = ['assets/speaker.png', 'assets/watch.png', 'assets/chair.jpg', 'assets/lamp.jpg', 'assets/home.jpg', 'assets/office.jpg'];
+List<String> itemName = ['Chair', 'Lamp', 'Home', 'Office'];
 
+List<String> itemImage = ['assets/chair.jpg', 'assets/lamp.jpg', 'assets/home.jpg', 'assets/office.jpg'];
 
   @override
   Widget build(BuildContext context) {
+    var data = MediaQuery.of(context);
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        backgroundColor: Color(0xffE74C3C),
-
-        title: InkWell(
-          child: Text(
-            'Shopping Cart',
-            style: TextStyle(fontFamily: 'Montserrat'),
-          ),
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Basket',
+          style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold)
         ),
-        centerTitle: true,
-
-        actions: [
-          IconButton(
-            icon: Icon(Icons.search,color: Colors.white,),
-            onPressed: (){},
-          ),
-        ],
       ),
-
-
-      backgroundColor: Colors.white,
-
+      backgroundColor: Colors.grey[100],
       body: Container(
-        margin: EdgeInsets.fromLTRB(0.0, 5.0, 15.0, 5.0),
-
-        // need revision.
-
+        margin: EdgeInsets.all(5.0),
         child: GridView.builder(
-          padding: EdgeInsets.only(top: 20.0),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-
             crossAxisCount: 2,
             mainAxisSpacing: 5.0,
-            childAspectRatio: 0.57
+            childAspectRatio: responsiveGridViewItem(data)
           ),
-
           scrollDirection: Axis.vertical,
-
           itemCount: itemName.length,
-          itemBuilder: (context,index){
-            return Item(itemImage[index], itemName[index],'description','hdf','!order',200.0);
+          itemBuilder: (context, index){
+            return Item(itemImage[index], itemName[index], 'description', 200.0, '!order', 'hdf');
           },
-        ),
-      ),
-
-      bottomNavigationBar: Container(
-        child: Row(
-          children: [
-            Expanded(
-              child: ListTile(
-                title: Text(
-                  'Total',style: TextStyle(
-                    color: Color(0xffE74C3C),fontSize: 17.5,fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-            ),
-
-            Expanded(
-              child: MaterialButton(
-                onPressed: () {},
-                child: Text(
-                  'Buy',
-                  style: TextStyle(color: Colors.white),
-                ),
-                color: Color(0xffE74C3C),
-              ),
-            )
-          ],
         ),
       ),
     );
