0ae5462c31b9feb2da7a7b01614b417764f6e196
Hussein AL-zomar <68166543+husseinzomar@users.noreply.github.com>
2020-11-20 22:14:25 +0200

    Update homapage.dart

--- homapage.dart
+++ homapage.dart
@@ -1,243 +1,369 @@
+import 'package:intel/models/item/itemcontroller.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:floating_search_bar/floating_search_bar.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
-import 'package:intel/components/CardGridNav.dart';
-import 'package:intel/components/webview.dart';
-import 'package:intel/dao/home_dao.dart';
-import 'package:intel/model/common_model.dart';
-import 'package:intel/model/home_model.dart';
-import 'package:intel/components/grid_nav.dart';
-import 'package:intel/model/grid_nav_model.dart';
-import 'package:intel/components/sub_nav.dart';
-import 'package:intel/components/sales_box.dart';
-import 'package:intel/model/sales_box_model.dart';
-import 'package:intel/components/loading_widget.dart';
-import 'package:intel/components/search_bar.dart';
-import 'package:intel/pages/search_page.dart';
-import 'package:intel/pages/speak_page.dart';
-import 'dart:convert';
-import 'dart:async';
+import 'dart:io';
+import 'dart:io' show Platform;
+import 'package:intel/bottomnavbar/shoppingcart.dart';
+import 'package:intel/bottomnavbar/seeAll.dart';
+import 'package:intel/widgets/loading.dart';
+import 'package:intel/widgets/item.dart';
 
-const APPBAR_SCROLL_OFFSET = 100;
+
+
 
 
 class HomePage extends StatefulWidget {
+
+  final ItemController item;
+  HomePage(this.item);
+
   @override
-  State<StatefulWidget> createState() {
-    return _HomePageState();
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with AutomaticKeepAliveClientMixin {
-  double appBarOpacity = 0;
+class _HomePageState extends State<HomePage> {
 
-  String resultString = "";
+  List<String>categoryName = ['Man','Woman','Devices','Gadgets','Gaming'];
+  List<String>bastSellName = ['Beo Speker','Wristwatch','Lamp','Office'];
+  List<String>bastSellImage = ['assets/speaker.png','assets/watch.png','assets/lamp.jpg','assets/chair.jpg'];
+  List<String>bestSellingDesc = ['Ban&olu', 'Make Wrist, Smart','Office Lamp','Full office'];
+  List<double> bestSellingPrice=[199.9, 499.9, 99.9, 2000.0];
+  List<String>itemIcons = ['assets/men.png', 'assets/women.png', 'assets/devices.png', 'assets/gadgets.png', 'assets/gaming.png'];
+  List<String>itemPics = ['assets/speaker.png','assets/watch.png'];
+  List<String>newArrialName = ['Home','Lamp','Chair','Office',];
+  List<String>newArrialImage = ['assets/home.jpg','assets/lamp.jpg','assets/chair.jpg','assets/office.jpg'];
 
-  List<CommonModel> gridNavList;
-  List<CommonModel> subNavList;
-  SalesBoxModel salesBoxModel;
-  GridNavModel girdModeList;
-  List<CommonModel> bannerList;
-  bool isLoading = true;
 
   @override
-  void initState() {
+  void iniState(){
+    widget.item.getItems();
     super.initState();
-    _handleRefresh();
   }
 
-  @override
-  // implement wantKeepAlive true 
-  bool get wantKeepAlive => true;
+    File pickedImage;
 
-  Future<Null> _handleRefresh() async {
-  
-    
-    try {
-      HomeModel model = await HomeDao.fetch();
-      setState(() {
-        gridNavList = model.localNavList;
-        girdModeList = model.gridNav;
-        subNavList = model.subNavList;
-        salesBoxModel = model.salesBox;
-        bannerList = model.bannerList;
-        isLoading = false;
-        print(json.encode(model.localNavList));
-      });
-    } catch (e) {
-      print(e.toString());
-      setState(() {
-        isLoading = false;
-      });
-    }
-    return null;
-  }
-
-  
-  _scroll(offest) {
-    double alpha = offest / APPBAR_SCROLL_OFFSET;
-    if (alpha < 0) {
-      alpha = 0;
-    } else if (alpha > 1) {
-      alpha = 1;
-    }
-    setState(() {
-      appBarOpacity = alpha;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xfff2f2f2),
-        //appbar
-        body: LoadingWidget(
-          isLoading: isLoading,
-          child: Stack(
-            children: <Widget>[
-              //view 
-              
-              MediaQuery.removePadding(
-                removeTop: true, // padding
-                context: context,
-                child: RefreshIndicator(
-                    child: NotificationListener(
-                      // list view
-                      onNotification: (scrollNotification) {
+      backgroundColor: Colors.white,
+      body: Container(
+        child: ListView(
+          padding: EdgeInsets.only(top: 40.0),
 
-                        if (scrollNotification is ScrollUpdateNotification &&
-                            scrollNotification.depth == 0) {
-                          
-                          _scroll(scrollNotification.metrics.pixels);
-                        }
-                      },
-                      child: _buildListView,
-                    ),
-                    onRefresh: _handleRefresh),
+          scrollDirection: Axis.vertical,
+
+          children: [
+            ListTile(
+              //starting search bar 
+              title: Container(
+                padding: EdgeInsets.fromLTRB(10.0, 10.0, 0.0, 10.0),
+                decoration: BoxDecoration(
+                  color: Colors.grey[100],
+                  borderRadius: BorderRadius.circular(50.0),
+                ),
+
+                alignment: Alignment.centerLeft,
+                child: Icon(
+                  Icons.search,
+                  color: Color(0xffE74C3C),
+                  size: 22.0,
+                ),
               ),
-              //bar
-              _buildTopBar(appBarOpacity),
-            ],
-          ),
-        ));
-  }
 
-  ///bar
-  Widget _buildTopBar(appBarOpacity) {
-    return Column(
-      children: <Widget>[
-        Container(
-          
-          decoration: BoxDecoration(
-              gradient: LinearGradient(
-            colors: [Color(0x66000000), Colors.transparent],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          )),
-          child: Container(
-            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-            height: 80,
-            decoration: BoxDecoration(
-              color:
-                  Color.fromARGB((appBarOpacity * 255).toInt(), 255, 255, 255),
+              // drawing the circle behind cameraicon
+              trailing: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Color(0xffE74C3C),
+                  shape: BoxShape.circle,// draw circle
+                ),
+
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.camera_alt,
+                      size: 20.0,
+                    ),
+                    onPressed: (){
+                      showModalBottomSheet(
+                        context: context,
+                        backgroundColor: Colors.white,
+                        builder: (BuildContext context){
+
+                          return Container(
+                            margin: EdgeInsets.all(10.0),
+                            height: MediaQuery.of(context).size.height,
+                            width: MediaQuery.of(context).size.width,
+
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text(
+                                    'Camera',
+                                    style: TextStyle(color: Color(0xffE74C3C),
+                                    fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                  trailing: Icon(Icons.camera_alt,color: Color(0xffE74C3C),size: 20.0,),
+                                  onTap: (){
+                                    pickImage(ImageSource.gallery);
+                                  }
+                                )
+                              ],
+                            ),
+                          );
+                        }
+                      );
+                    },
+                  ), 
+                ),
             ),
-            child: SearchBar(
-              searchType:
-                  appBarOpacity > 0.2 ? SearchType.homeLight : SearchType.home,
-              inputBoxClick: _jumpToSearch,
-              speakClick: _jumpToSpeak,
-              defultText: 'jump',
-              leftButtonClick: () {},
-              rightButtonClick: () {},
+            //end of the saerch bar 
+
+            ListTile(
+              title: Text(
+                'Categories',
+                style: TextStyle(
+                  color: Color(0xffE74C3C),
+                  fontSize: 20.0,
+                  fontFamily: 'MontserratSemiBold'
+                ),
+              ),
+              trailing: Text(
+                'See All',
+                style: TextStyle(
+                  color: Color(0xffE74C3C),
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.normal
+                ),
+              ),
             ),
-          ),
+            //categories icons 
+
+            Container(
+              height: 67.0,
+              child: ListView.builder(//important
+                scrollDirection: Axis.horizontal,
+                itemCount: itemIcons.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                     width: 80.0,
+                     child: ListTile(
+                       title: Image.asset(itemIcons[index]),
+                       subtitle: Text(
+                         categoryName[index],
+                         textAlign: TextAlign.center,
+                         style: TextStyle(
+                           color: Color(0xffE74C3C),
+                           fontSize: 11.2,
+                           fontFamily: 'Montserrat'
+                         ),
+                       ),
+                     ),
+                  );
+                },
+              ),
+            ),
+
+
+            ListTile(
+              title: Text(
+                'Bast Selling',
+                style: TextStyle(
+                  color: Color(0xffE74C3C),
+                  fontSize: 20.0,
+                  fontFamily : 'MontserratSemiBold'
+                ),
+              ),
+              trailing: Text(
+                'See All',
+                style: TextStyle(
+                  color: Color(0xffE74C3C),
+                  fontSize: 15.0,
+                  fontFamily: 'MontserratSemiBold',
+                ),
+              ),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (_) {return SearchResult('Best Selling');}));
+              },
+            ),
+
+
+            Container(
+              height: MediaQuery.of(context).size.height/2.6,
+              child: bastSellingItem(),
+            ),
+
+            ListTile(
+              title: Text(
+                'Featured Brands',
+                style: TextStyle(color: Color(0xffE74C3C),
+                  fontSize: 20.0,
+                    fontFamily: 'MontserratSemiBold'
+                ),
+              ),
+            ),
+           
+           Container(
+             height: 50.0,
+             decoration: BoxDecoration(
+               image: DecorationImage(
+                 image: AssetImage('assets/brands.png'),
+                 fit: BoxFit.cover
+               ),
+             ),
+           ),
+
+
+           Container(
+             margin: EdgeInsets.only(top: 20.0),
+             height: 200.0,
+             decoration: BoxDecoration(
+               image: DecorationImage(
+                 image: AssetImage('assets/mobheader.png')
+               )
+             ),
+           ),
+
+           Container(
+             padding: EdgeInsets.only(top: 20.0),
+           ),
+
+
+           ListTile(
+             title: Text(
+               'Recommended',
+               style: TextStyle(
+                 color: Color(0xffE74C3C),
+                 fontSize: 20.0,
+                 fontFamily: 'MontserratSemiBold'
+               ),
+             ),
+             trailing: Text(
+               'See All',
+               style: TextStyle(
+                 color: Color(0xffE74C3C),
+                 fontSize: 15.0,
+                 fontWeight: FontWeight.normal
+               ),
+             ),
+             onTap: (){
+               Navigator.push(context, MaterialPageRoute(builder: (_) {return SearchResult('New Arrival');}));
+             },
+           ),
+
+
+           Row(children: [
+             Container(
+               margin: EdgeInsets.fromLTRB(15, 0, 15, 10),
+               height: MediaQuery.of(context).size.height/3,
+               width: MediaQuery.of(context).size.width/2.3,
+
+               decoration: BoxDecoration(
+                 image: DecorationImage(
+                   image: AssetImage('hussein.jpg'),
+                   fit: BoxFit.cover 
+                   ),
+                   borderRadius: BorderRadius.circular(7.5),
+               ),
+             ),
+
+
+             Container(
+               margin: EdgeInsets.fromLTRB(0, 0, 15, 10),
+               height: MediaQuery.of(context).size.height/3,
+               width: MediaQuery.of(context).size.width/2.3,
+
+               decoration: BoxDecoration(
+                 image: DecorationImage(
+                   image: AssetImage('pp.jpg'),
+                   fit: BoxFit.cover 
+                   ),
+                   borderRadius: BorderRadius.circular(7.5),
+               ),
+             )
+           ]
+           ),
+
+
+
+            ListTile(
+                title: Text('Best Sellings',
+                  style: TextStyle(color: Color(0xff007386),
+                    fontSize: 20.0, fontFamily: 'MontserratSemiBold',),
+                ),
+                trailing: Text('See All',
+                  style: TextStyle(color: Color(0xff007386), fontSize: 15.0, fontWeight: FontWeight.normal),
+                ),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {return SearchResult('New Arrival');}));
+                },
+              ),
+
+              Container(
+                height: MediaQuery.of(context).size.height/2.4,
+                child:homaPageItem(false) ,
+              )
+          ],
         ),
-
-        ///
-        Container(
-          height: appBarOpacity > 0.2 ? 0.5 : 0,
-          decoration: BoxDecoration(
-            boxShadow: [BoxShadow(color: Colors.black12, blurRadius: 0.5)],
-          ),
-        )
-      ],
-    );
-  }
-
-  _jumpToSearch() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => SearchPage(
-              hint: 'Internet celebrity check-in locations, attractions, hotels, food',
-            ),
       ),
     );
   }
+  homaPageItem(bool isBestSelling){
 
-   _jumpToSpeak() {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => SpeakPage()));
-  }
 
-  
-  ListView get _buildListView {
-    return ListView(
-      children: <Widget>[
-        _buildBanner,
-        Padding(
-          padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-          child: GridNav(gridNavList: gridNavList),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-          child: CardGridNav(
-            gridNavModel: girdModeList,
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-          child: SubNav(
-            subNavList: subNavList,
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-          child: SalesBox(
-            salesBoxModel: salesBoxModel,
-          ),
-        ),
-      ],
-    );
-  }
-
-  /// banner
-  Widget get _buildBanner {
-    return Container(
-      height: 160.0,
-      child: Swiper(
-        itemCount: bannerList == null ? 0 : bannerList.length,
-        autoplay: true,
-        itemBuilder: (BuildContext context, int index) {
-          return GestureDetector(
-            onTap: () {
-              CommonModel model = bannerList[index];
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => WebView(
-                            url: model.url,
-                            title: model.title,
-                            statusBarColor: model.statusBarColor,
-                            hideAppBar: model.hideAppBar,
-                          )));
-            },
-            child: Image.network(bannerList[index].icon,
-                fit: BoxFit.fill), ,
+    return ScopedModelDescendant<ItemController>(
+      builder: (context,child, ItemController item){
+       if(item.isProductLoading == true){
+            return Center(child: Loading());
+          }else if(item.alliItems.length < 1){
+            return Center(child: Text('No items found.'));
+          }else{
+            return ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: item.alliItems.length,
+                itemBuilder: (context, index){
+                  return Item(
+                      bastSellImage[index],
+                      item.alliItems[index].itemName,
+                      item.alliItems[index].itemDescription,
+                      item.alliItems[index].id,
+                      '!order',
+                      item.alliItems[index].itemPrice
+                  );
+                }
+            );
+          }
+        }
           );
-        },
-        pagination: SwiperPagination(), 
-      ),
-    );
-  }
-}
+        }
+     bastSellingItem(){
+       return ListView.builder(
+         scrollDirection: Axis.horizontal,
+
+         itemCount: bastSellName.length,
+         itemBuilder: (context, index){
+           return Item(bastSellImage[index],bastSellName[index],bestSellingDesc[index],'hdf','order',bestSellingPrice[index]);
+         },
+       );
+     }
+
+     newArrivalItem(){
+       return ListView.builder(
+         scrollDirection: Axis.horizontal,
+
+         itemCount: newArrialName.length,
+         itemBuilder: (context,index){
+           return Item(newArrialImage[index],newArrialName[index],'description', 'hdf','!order',200.0);
+         },
+       );
+     }
+      
+  pickImage(ImageSource source)async{
+    var _pickedImage = await ImagePicker.pickImage(source: source);
+    setState(() {
+      pickedImage = _pickedImage;
+  });
+ }
+} 
